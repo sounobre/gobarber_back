0f8f96778860ecca812e631087e23e3e217c8c49
Diego Nobre <sounobre@hotmail.com>
2021-01-20 09:43:10 -0300

    Subindo restante do projeto

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -1,19 +1,50 @@
-import multer from 'multer';
-import crypto from 'crypto';
 import path from 'path';
+import crypto from 'crypto';
+import multer, { StorageEngine } from 'multer';
+
+interface IUploadConfig {
+  driver: 'disk' | 's3';
+
+  tmpFolder: string;
+  uploadsFolder: string;
+
+  multer: {
+    storage: StorageEngine;
+  };
+
+  config: {
+    disk: {};
+
+    aws: {
+      bucket: string;
+    };
+  };
+}
 
 const tmpFolder = path.resolve(__dirname, '..', '..', 'tmp');
 
 export default {
-  directory: tmpFolder,
+  driver: process.env.STORAGE_DRIVER,
 
-  storage: multer.diskStorage({
-    destination: tmpFolder,
-    filename(request, file, callback) {
-      const fileHash = crypto.randomBytes(10).toString('hex');
-      const filename = `${fileHash}-${file.originalname}`;
+  tmpFolder,
+  uploadsFolder: path.resolve(tmpFolder, 'uploads'),
 
-      return callback(null, filename);
+  multer: {
+    storage: multer.diskStorage({
+      destination: tmpFolder,
+      filename(request, file, callback) {
+        const filehash = crypto.randomBytes(10).toString('hex');
+        const fileName = `${filehash}-${file.originalname}`;
+
+        return callback(null, fileName);
+      },
+    }),
+  },
+
+  config: {
+    disk: {},
+    aws: {
+      bucket: 'app-gobarber-2',
     },
-  }),
-};
+  },
+} as IUploadConfig;
